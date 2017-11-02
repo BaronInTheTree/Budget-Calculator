fc67ed81d0cf7b2808e6b949e067b9026a006fbc
Song Xiao <sxa17@sfu.ca>
2017-11-02 15:32:42 -0700

    create constructors, data object and addItem function in budgetController.

--- app.js
+++ app.js
@@ -1,7 +1,57 @@
+
 // budget controller
 var budgetController = (function(){
 
-	// functions to add
+	var Income = function(id, description, value){
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var Expense = function(id, description, value){
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+	
+	var data = {
+		allItems: {
+			exp: [],
+			inc: []
+		},
+		totals: {
+			exp: 0,
+			inc: 0
+		}
+	};
+
+	return {
+		addItem: function (type, des, val) {
+			var items, ID, newItem;
+
+			items = data.allItems[type];
+			
+			// create new ID. Ensure the new item ID is last item's ID + 1.
+			if (items.length > 0) ID = items[items.length-1].id + 1;
+			else ID = 0;
+
+			if (type === "inc") {
+				newItem = new Income (ID, des, val);
+			}
+			else if (type === "exp"){
+				newItem = new Expense (ID, des, val);
+			}
+
+			data.allItems[type].push(newItem);
+			
+			return newItem;
+		},
+
+		testing: function () {
+			console.log(data);
+		}
+	}
+
 
 })();
 
@@ -34,15 +84,29 @@ var UIController = (function(){
 // Global App controller 
 var controller = (function(budgetCtrl, UICtrl){
 	
-	var DOM = UICtrl.getDOMstrings();
+	// set up event listeners and get strings 
+	var setupEventListeners = function(){
+		
+		var DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+		document.addEventListener("keypress", function(event){
+			if (event.keyCode === 13 || event.which === 13) { // to support old browser without keycode
+				ctrlAddItem();
+			}
+		});
+	};
 
 	// function to add new item
 	var ctrlAddItem = function () {
+		var input, newItem;
 
 		// 1. get input data 
-		var input = UICtrl.getinput();
+		input = UICtrl.getinput();
 
 		// 2. add item to the budget controller
+		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
 		// 3. add the item to the UI
 
@@ -52,12 +116,18 @@ var controller = (function(budgetCtrl, UICtrl){
 
 	}
 
-	document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+	return {
+		// initialization function
+		init: function(){
+			setupEventListeners();
 
-	document.addEventListener("keypress", function(event){
-		if (event.keyCode === 13 || event.which === 13) { // to support old browser without keycode
-			ctrlAddItem();
-		}
-	});
+		},
+
+
+
+	};
+
 
 })(budgetController,UIController);
+
+controller.init();
