15e33cb76b952452b7ccdad093faf0d9c057c4c5
Song Xiao <sxa17@sfu.ca>
2017-11-04 11:40:05 -0700

    add functionality on displaying budgets, creating event handler to delete items.

--- app.js
+++ app.js
@@ -70,6 +70,20 @@ var budgetController = (function(){
 			}
 		},
 
+		deleteItem: function(type, id) {
+			var ids, index;
+
+			var ids = data.allItems[type].map(function(current){
+				return current.id;
+			});
+
+			index = ids.indexOf(id);
+
+			if (index !== -1) {
+				data.allItems[type].splice(index,1);
+			}
+		},
+
 		testing: function () {
 			console.log(data);
 		}
@@ -88,7 +102,12 @@ var UIController = (function(){
 		inputValue: ".add__value",
 		inputBtn: ".add__btn",
 		incomeContainer : ".income__list",
-		expenseContainer: ".expenses__list"
+		expenseContainer: ".expenses__list",
+		budgetLabel: ".budget__value",
+		incomeLabel: ".budget__income--value",
+		expensesLabel: ".budget__expenses--value",
+		percentageLabel: ".budget__expenses--percentage",
+		container:".container"
 	};
 
 	return {
@@ -111,13 +130,13 @@ var UIController = (function(){
 			// create HTML string with placeholder text
 			if (type === 'inc'){
 				element = DOMstrings.incomeContainer;
-				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div>' + 						
+				html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div>' + 						
 				'<div class="right clearfix"><div class="item__value">%value%</div>' +
 				'<div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 			}
 			else if (type === 'exp'){
 				element = DOMstrings.expenseContainer;
-				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div>' + 
+				html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div>' + 
                    '<div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div>' +
                    '<div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 			}
@@ -139,6 +158,19 @@ var UIController = (function(){
 
 			// set focus back to description
 			document.querySelector(DOMstrings.inputDescription).focus();
+		},
+
+		displayBudget: function(obj){
+			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+			if (obj.percentage > 0){
+				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + "%";
+			}
+			else {
+				document.querySelector(DOMstrings.percentageLabel).textContent = "---";
+			}
 		}
 	}
 
@@ -159,6 +191,8 @@ var controller = (function(budgetCtrl, UICtrl){
 				ctrlAddItem();
 			}
 		});
+
+		document.querySelector(DOM.container).addEventListener("click", ctrlDeleteItem);
 	};
 
 	var updateBudget = function() {
@@ -170,7 +204,7 @@ var controller = (function(budgetCtrl, UICtrl){
 		var budget = budgetCtrl.getBudget();
 
 		// 3. display budget on the UI
-		console.log(budget);
+		UICtrl.displayBudget(budget);
 
 	};
 
@@ -195,12 +229,37 @@ var controller = (function(budgetCtrl, UICtrl){
 			updateBudget();
 
 		}
-	}
+	};
+
+	var ctrlDeleteItem = function(event) {
+		var itemID, splitID, type, ID;
+		// find the id of item-clearfix: i.e. "inc-0" or "exp-2" ...
+		itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+	
+		if (itemID) {
+			splitID = itemID.split("-");
+			type = splitID[0];
+			ID = parseInt(splitID[1]);  // to convert string into Integer
+
+			// 1. delete item from data structure 
+			budgetCtrl.deleteItem(type,ID);
+
+			// 2. delete item from UI
+
+			// 3. update and show and new budget
+		}
+	};
 
 	return {
 		// initialization function
 		init: function(){
 			setupEventListeners();
+			UICtrl.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 		},
 
 	};
